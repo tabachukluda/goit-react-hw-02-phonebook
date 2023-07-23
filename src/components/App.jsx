5f0ebaccd9558266a0d70bf91a5c2f1ceb262ed5
tabachukluda <121580942+tabachukluda@users.noreply.github.com>
2023-07-23 11:53:53 +0200

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,94 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const id = nanoid();
+    const name = this.state.name;
+    const number = this.state.number;
+    const contactsLists = [...this.state.contacts];
+
+    if (!name || !number) {
+      alert("Please provide a name and a number.");
+      return;
+    }
+
+    if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      contactsLists.push({ name, id, number });
+      this.setState({ contacts: contactsLists, name: '', number: '' });
+    }
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  getFilteredContacts = () => {
+    const filterContactsList = this.state.contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(this.state.filter.toLowerCase());
+    });
+
+    return filterContactsList;
+  };
+
+  render() {
+    const { filter, name, number } = this.state;
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm
+          name={name}
+          number={number}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <h2>Contacts</h2>
+        <Filter filter={filter} handleChange={this.handleChange} />
+        {this.state.contacts.length === 0 ? (
+          <p>No contacts to display.</p>
+        ) : (
+          <ContactList
+            contacts={this.getFilteredContacts()}
+            handleDelete={this.handleDelete}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
